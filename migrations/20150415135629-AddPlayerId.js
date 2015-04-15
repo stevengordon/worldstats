38a729dc8168ffe75464ae3c570d85b77169fcdc
Steven Gordon <steven@SRG-MBP.local>
2015-04-15 14:09:23 -0700

    Sequelize migrations to add columns to Scores

---
+++ migrations/20150415135629-AddPlayerId.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+
+    migration.addColumn(
+    	'Scores',
+    	'PlayerId',
+    	DataTypes.INTEGER
+	)
+    done();
+  },
+
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    done();
+  }
+};
